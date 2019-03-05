b18fc2a8341c2a2076dae2f55d7858deffd0f0ce
unknown <tinnarut96@gmail.com>
2019-03-05 10:00:43 +0000

    Little bit added to Elevator.java

--- src/com/ru/usty/elevator/Elevator.java
+++ src/com/ru/usty/elevator/Elevator.java
@@ -1,5 +1,21 @@
 package com.ru.usty.elevator;
 
-public class Elevator {
+public class Elevator implements Runnable {
+	
+	int numberFloors, maxOccupants;
+	Elevator(int numberFloors, int maxOccupants) {
+		this.numberFloors = numberFloors;
+		this.maxOccupants = maxOccupants;
+	}
 
+	@Override
+	public void run() {
+		try {
+			ElevatorScene.sem.acquire();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Elevator Thread");
+	}
+	
 }
