902ec8608951a24e03a671205673de94df6f78ee
Tinna <tinnarut96@gmail.com>
2019-03-05 09:59:52 +0000

    Revert "Smá bætt við Elevator.java"
    This reverts commit 0568c59ed8a76bb521227d7cb62540de69a962fb.

--- src/com/ru/usty/elevator/Elevator.java
+++ src/com/ru/usty/elevator/Elevator.java
@@ -1,21 +1,5 @@
 package com.ru.usty.elevator;
 
-public class Elevator implements Runnable {
-	
-	int numberFloors, maxOccupants;
-	Elevator(int numberFloors, int maxOccupants) {
-		this.numberFloors = numberFloors;
-		this.maxOccupants = maxOccupants;
-	}
+public class Elevator {
 
-	@Override
-	public void run() {
-		try {
-			ElevatorScene.sem.acquire();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		System.out.println("Elevator Thread");
-	}
-	
 }
